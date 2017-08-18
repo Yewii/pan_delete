ae61104c99bec772b6f03d58f15282ea49700f47
Yewii <894250298@qq.com>
2017-08-18 16:50:00 +0800

    实现文件url

--- utf8.py
+++ utf8.py
@@ -1,9 +1,21 @@
 #-*-coding:utf8-*-
 str='来自：Z9 Max'
 res = str.encode('utf-8')
+res=repr(res)
 
 
+res = res[res.find("b") + 1:]
+
+import re
+strinfo = re.compile('\\\\x')
+res = strinfo.sub('%',res)
+strinfo = re.compile(' ')
+res = strinfo.sub('%20',res)
+res=eval(res)
+
 print(res)
-res=res.decode('utf-8')
-print(type(res))
-print(res)
+
+url0='https://pan.baidu.com/disk/home?errno=0&errmsg=Auth%20Login%20Sucess&&bduss=&ssnerror=0#list/vmode=grid&path=%2F'
+
+url1=url0+res
+print(url1)
